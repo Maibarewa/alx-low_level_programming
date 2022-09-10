1dfa465eed486df3bbaf84df82ff36c232f14dff
Maibarewa <najibusmanalasan@gmail.com>
2022-09-10 09:38:34 -0700

    Hello, printf

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,25 +1,24 @@
 #include <stdio.h>
 
 **/
-* main - print in the sizeof
+* main - print the string in the function
 *
-* Description: Printing Size of function
-* This program print various sizeof function
-* Return: 0
+* Description: usnig the main function
+* This program prints "The main function
+* Return: 0;
 */
 int main(void)
 {
 	char c;
 	int i;
-	long l;
-	long long ll;
+	long li;
+	long long lli;
 	float f;
 
-	printf("Size of a char: %c byte(s)\n", sizeof(c));
+	printf("Size of a char: %d byte(s)\n", sizeof(c));
 	printf("Size of an int: %d byte(s)\n", sizeof(i));
-	printf("Size of long: %d byte(s)\n", sizeof(l));
-	printf("Size of long long: %d(s)\n", sizeof(ll));
-	printf("Size of float: %f byte(s)\n", sizeof(f));
+	printf("Size of long int: %d byte(s)\n", sizeof(li));
+	printf("Size of long long int: %d byte(s)\n", sizeof(lli));
+	printf("Size of float: %d byte(s)\n", sizeof(f));
 	return (0);
-
 }
