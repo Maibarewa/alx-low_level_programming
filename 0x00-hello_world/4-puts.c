e3769210f909f36b7b3b75006e09ce93a838683a
Maibarewa <najibusmanalasan@gmail.com>
2022-09-10 02:08:54 -0700

    Printing

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,20 +1,13 @@
 #include <stdio.h>
 
 /**
- *
- * main -Entry point
- *
- *   
- *
- * Return: Always 0 (Success)
- *
- */
-
+*
+* main - Entry point
+* 
+* Return: Always 0 (Success)
+*/
 int main(void)
-
 {
-
-	puts("\"Programming is like building a multilingual puzzle");
-
+	puts("\"Progrmming is like building a multilimual puzzle");
+	
 	return (0);
-}
