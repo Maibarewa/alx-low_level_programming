60b2a87c25e70bd6e8a4b4c2ac2c117875d11187
Maibarewa <najibusmanalasan@gmail.com>
2022-09-10 23:38:30 -0700

    size of printing

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 
 /**
-* main -Entry point
+* main - Entry point
 *
 * Return: Always 0 (success)
+*
 */
 int main(void)
 {
@@ -13,10 +14,10 @@ int main(void)
 	char d;
 	float f;
 
-	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
 	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
-	printf("Size of long int: %lu byte(s)\n",(unsigned long)sizeof(b));
-	printf("Size of long long int: %lu byte(s)\n",(unsigned long)sizeof(c));
-	print("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
- 	return (0);
+	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+	return (0);
 }
