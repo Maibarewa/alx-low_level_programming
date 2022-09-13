85949dfa62037c849a64d59b46aca96bf653d12b
Maibarewa <najibusmanalasan@gmail.com>
2022-09-12 23:24:18 -0700

    I sometimes suffer from insomnia

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints alphabet in lower case
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char letter 
+	
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter); 
+
+	putcahar('\n');
+
+	return (0);
+}
