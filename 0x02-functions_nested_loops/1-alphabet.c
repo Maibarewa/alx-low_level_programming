250445cba941a0c62743c2d4341d19ba80d63ebc
Maibarewa <najibusmanalasan@gmail.com>
2022-09-14 04:23:24 -0700

    I sometimes suffer from insomnia

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print alphabet - function to print a-z
+ *
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+	char c;
+	
+	for  (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+_putchar('\n');
+}
