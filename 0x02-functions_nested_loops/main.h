89b2d8312d1ca13608aea2d097627321a270153c
Maibarewa <najibusmanalasan@gmail.com>
2022-09-14 03:31:34 -0700

    putchar

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,8 @@
 #ifndef HOLBERTON_H_INCLUDED
 #define HOLBERTON_H_INCLUDED
+int _putchar(char);
+void print_alphabet(void);
+void print_alphabet_10(void);
 int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
