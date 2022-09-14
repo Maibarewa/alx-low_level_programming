c8a651428ef261265c803da09de84e53aadf106a
Maibarewa <najibusmanalasan@gmail.com>
2022-09-14 07:30:09 -0700

    lower case 10 times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
- * print_laphabet_x10 - function to print abc times ten 
- * Description: this program print lowercase ten times
+ * print_alphabet_x10 - function to print abc 10 times
+ *
  * Return: 0
  */
 void print_alphabet_x10(void)
