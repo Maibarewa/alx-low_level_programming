5cd677945708db388e2caab19229aa3b9bbb3166
Maibarewa <najibusmanalasan@gmail.com>
2022-09-14 06:04:46 -0700

    printing lower case

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,18 +1,18 @@
 #include "main.h"
 
 /**
- * print alphabet - function to print a-z
+ * print_alphabet - function to print abcd
  *
- * Description: this program print alphabet in lower case
  * Return: 0
  */
 void print_alphabet(void)
 {
 	char c;
-	
-	for  (c = 'a'; c <= 'z'; c++)
+
+	for (c = 'a'; c <= 'z'; c++)
 	{
-	 _putchar(c);
+		_putchar(c);
 	}
-_putchar('\n');
+-putchar('\n');
 }
+
