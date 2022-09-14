b9d1964fa92aaf82914ab2640fed33d460c9c9e8
Maibarewa <najibusmanalasan@gmail.com>
2022-09-14 07:04:25 -0700

    lowercase

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,7 +2,7 @@
 #define HOLBERTON_H_INCLUDED
 int _putchar(char);
 void print_alphabet(void);
-void print_alphabet_10(void);
+void print_alphabet_x10(void);
 int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
