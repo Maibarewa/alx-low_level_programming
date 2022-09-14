a8c39d938e32d7ed7688f937686d430edc58bb81
Maibarewa <najibusmanalasan@gmail.com>
2022-09-14 10:30:32 -0700

    jack bauer

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -15,8 +15,8 @@ void jack_bauer(void)
 	{
 		while (minutes <= 59)
 		{
-			mins_remainder % 10;
-			hours_remainder % 10;
+			mins_remainder = minutes % 10;
+			hours_remainder = hours % 10;
 			_putchar(hours / 10 + '0');
 			_putchar(hours_remainder + '0');
 			_putchar(':');
