fc6e4ab18938e2c6c8747ef5c0564e2513ecc4ea
Maibarewa <najibusmanalasan@gmail.com>
2022-09-16 03:25:36 -0700

    multiple of two number

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <main.h>
+/**
+ * mul - multiplies two numbers
+ * @a: first number
+ * @b: second number
+ * Return: return result
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
