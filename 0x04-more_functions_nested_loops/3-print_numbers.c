dc37efdb5be61465970a71df9ed85826bfed8697
Maibarewa <najibusmanalasan@gmail.com>
2022-09-16 04:03:44 -0700

    from 1 to 0

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_numbers - function that print all numbers form 0 to 9
+ * Return: return void
+ */
+void print_numbers(void)
+{
+	int i
+	
+	for (i = 0; i < 10; i++)
+	{
+		_putchar(i);
+		_putchar('\n');
+	}
+
+	return (0);
+}
+
