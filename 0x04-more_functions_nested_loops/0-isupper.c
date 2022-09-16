1bf42507050abb0b4762d37e5a4092f0b5db16b9
Maibarewa <najibusmanalasan@gmail.com>
2022-09-16 03:06:28 -0700

    upper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,9 +1,8 @@
 #include "main.h"
-
 /**
- * _isupper - checks if a character isuppercase or not
- * @C: character to be tested
- * Return: 1 if true, 0 if false
+ * _isupper - checks if the characteris upper or not
+ * @c: chracter to be tested
+ * Return: 1 weather it is, 0 otherwise
  */
 int _isupper(int c)
 {
@@ -11,5 +10,6 @@ int _isupper(int c)
 	{
 		return (1);
 	}
-		return (0);
+	return (0);
 }
+
