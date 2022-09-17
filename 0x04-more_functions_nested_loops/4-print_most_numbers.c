c392c938b12ea1b312cfb730e8696e538f6289dc
Maibarewa <najibusmanalasan@gmail.com>
2022-09-17 09:11:08 -0700

    bleive numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -4,13 +4,13 @@
 /**
  * print_most_numbers - print numbers except 2 and 4
  *
- * Return: returnns nothing
+ * Return: returns nothing
  */
 void print_most_numbers(void)
 {
 	int n;
 
-	for  (n = 48; n < 58; n++)
+	for (n = 48; n < 58; n++)
 	{
 		if ((n == 50) || (n == 52))
 		{
