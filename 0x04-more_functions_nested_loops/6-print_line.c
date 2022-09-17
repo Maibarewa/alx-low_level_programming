840db8dd5be6b6d24413140acf23bf92afb34258
Maibarewa <najibusmanalasan@gmail.com>
2022-09-17 07:48:46 -0700

    printing line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include <stdio>
+#include "main.h"
+
+/**
+ * print_line - print a straight line
+ * @n: prameters
+ * Return: return nothing
+ */
+void print_line(int n)
+{
+	while (n-- > 0)
+	{
+		putchar('_');
+	}
+	putchar('\n');
+}
+
