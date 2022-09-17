48eb621a5324a4432c7f7c5d175ff0ed2e99f030
Maibarewa <najibusmanalasan@gmail.com>
2022-09-17 08:51:28 -0700

    numbers speech

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -5,13 +5,14 @@
  * print_numbers - function that print all numbers form 0 to 9
  * Return: return void
  */
-void print_numbers(void)
+int print_numbers(void)
 {
 	int i;
-	
+
 	for (i = 48; i < 58; i++)
 	{
 		putchar(i);
 	}
 	putchar(10);
 }
+
