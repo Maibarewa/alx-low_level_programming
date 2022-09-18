01651851ae9144a7918dc54a72197d0b6ebb0416
Maibarewa <najibusmanalasan@gmail.com>
2022-09-18 07:06:46 -0700

    the lower not  e and q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - thsi program prints lower case except e and q
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char lower = 'a';
+
+	while (lower <= 'z')
+	{
+		if (lower == 'e' || lower == 'q')
+			++lower;
+			putchar(lower);
+			++lower;
+	}
+	putchar('\n');
+
+	return (0);
+}
