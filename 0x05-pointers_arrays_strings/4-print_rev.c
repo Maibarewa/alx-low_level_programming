3b01fff38be26bd70a8e64cc82d09beb2cb2a88c
Maibarewa <najibusmanalasan@gmail.com>
2022-09-19 10:11:27 -0700

    reverse string

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * print_rev - prints  a srting in reverse order
+ * @s: string to reverse
+ * return: Nothing
+ */
+void print_rev(char *s);
+{
+	int len = string(s);
+	while (len--)
+		putchar(*(s + len));
+	putchar(10);
+}
