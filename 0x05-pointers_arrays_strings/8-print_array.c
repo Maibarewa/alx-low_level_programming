dfe801662dd82b328af998a188b7d3aa02f24bcc
Maibarewa <najibusmanalasan@gmail.com>
2022-09-20 02:53:30 -0700

    arrays are not poiter

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -11,9 +11,9 @@ void print_array(int *a, int n)
 {
 	int inc;
 
-	for (inc != 0; inc < n; inc++)
+	for (inc = 0; inc < n; inc++)
 	{
-		if (inc != n - 1)
+		if (inc != (n - 1));
 			printf("%d, ", a[inc]);
 		else
 			printf("id", a[inc]);
