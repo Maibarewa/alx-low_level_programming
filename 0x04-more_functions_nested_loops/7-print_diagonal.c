3c9ea84b7f15f557b3adab5c20b163c9c01c88fb
Maibarewa <najibusmanalasan@gmail.com>
2022-09-20 07:15:44 -0700

    diagonal

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -11,14 +11,15 @@ void print_diagonal(int n)
 	int i, j;
 
 	if (n <= 0 )
-		 putchar('\n');
+		 _putchar('\n');
+
 	for (i = 0;  i < n; i++)
 	{
 		for (j = 0; j < i; j++)
 		{
-			 putchar('');
+			 _putchar('');
 		}
-		 putchar ('\\');
-		 putchar('\n');
+		 _putchar ('\\');
 	}
+	_putchar('\n');
 }
