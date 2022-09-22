b883ca6cfadb2d30e60d59bf48ecdd2b7013b12f
Maibarewa <najibusmanalasan@gmail.com>
2022-09-22 15:34:40 -0700

    reversing array

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -10,10 +10,10 @@ void reverse_array(int *a, int n)
 {
 	int i, c;
 
-	for (i = 0; (i < (n-1) / 2); i++)
+	for (i = 0; (i < (n - 1) / 2); i++)
 	{
 		c = a[i];
 		a[i] = a[n - 1 - i];
-		a[n- 1 - i] = c;
+		a[n - 1 - i] = c;
 	}
 }
