d4f541a9d52468ed5b050030d399837900ddc8e3
Maibarewa <najibusmanalasan@gmail.com>
2022-09-23 10:02:46 -0700

    string cat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,13 +1,14 @@
 #include "main.h"
 #include <string.h>
+
 /**
- * _strcat - This is a fuction strcat
+ * _strcat -  This is a fuction strcat
  * @dest: first parameter
  * @src: second parameter
  * Rturn: dest
  */
 char *_strcat(char *dest, char *src)
 {
-	strcat(dest,src);
+	strcat(dest, src);
 	return (dest);
 }
