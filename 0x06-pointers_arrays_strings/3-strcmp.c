015405b50d0098555a6a4a7981652cf2c860c1c3
Maibarewa <najibusmanalasan@gmail.com>
2022-09-23 06:24:39 -0700

    string compare

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,7 +9,15 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int result;
-	result = strcmp(s1, s2);
-	return (result);
+	int int i = 0;
+
+	while (s1[i] != '\0' && s2[i] != '\0')
+	{
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+		i++;
+	}
+	if (s1[i] == s2[i])
+		return (0);
+	return (s1[i] - s2[i]);
 }
