4121ceb7eb82d3e8253e9ace031ceccd57362c09
Maibarewa <najibusmanalasan@gmail.com>
2022-09-24 01:29:19 -0700

    Multiple main

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,4 +1,4 @@
-#include "main.c"
+#include "main.h"
 /**
  * positive_or_negative - this checks weather number is postive or negative
  * @n: checks sign
@@ -9,6 +9,7 @@ int main(void)
 	int i;
 
 	i = 98 % 2;
+
 	positive_or_negative(i);
 
 	return (0);
