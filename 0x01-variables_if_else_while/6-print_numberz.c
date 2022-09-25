c059353e0d9b8ec1b9f4a425593607fe08d70ca7
Maibarewa <najibusmanalasan@gmail.com>
2022-09-25 10:28:36 -0700

    numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stlib>
+
+/**
+ * main - the function print all digit number of base 10 starting from 0
+ * Return: 0
+ */
+int main(void)
+{
+	int i = 0;
+
+	while (i <= 9)
+	{
+		putchar(i + '0');
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
