d9f7dc06593dcd32dbbb97088e81fb61d4c194bf
Maibarewa <najibusmanalasan@gmail.com>
2022-09-25 14:30:37 -0700

    hexa

---
+++ 0x01-variables_if_else_while/8-print_base16.c8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Thsi program print all number of base 16
+ * Return 0
+ */
+int main(void)
+{
+	int n = 48;
+
+	while (n <= 102)
+	{
+		putchar(n);
+		if (n == 57)
+			n += 39;
+			++n;
+	}
+	putchar('\n');
+	return (0);
+}
+
