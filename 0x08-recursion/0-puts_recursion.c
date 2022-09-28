e71bc354ca3126d49b3e0cea4ef11efaca7c7349
Maibarewa <najibusmanalasan@gmail.com>
2022-09-28 14:34:49 -0700

    recursion put

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -10,12 +10,8 @@ void _puts_recursion(char *s)
 	if (*s == '\0')
 	{
 		_putchar('\n');
+		return;
 	}
-	else
-	{
-		
-		_putchar (s[0]);
-		_puts_recursion(s + 1);
-	}
-	return (0);
+	_putchar(*s);
+	_puts_recursion(*s + 1);
 }
