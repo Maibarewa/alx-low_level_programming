9608c7035d91e720518ac37491fed60c0319454a
Maibarewa <najibusmanalasan@gmail.com>
2022-09-29 08:43:01 -0700

    recursion str length

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - The function prints string length
+ * @s: The string to be printed
+ * Return: string
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	else
+	{
+		return (1 + _strlen_recursion(s + 1));
+	}
+}
+	
