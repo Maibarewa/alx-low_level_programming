affe484deded29c65d635f13893914d174da82bc
Maibarewa <najibusmanalasan@gmail.com>
2022-09-29 09:16:37 -0700

    powerof y

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - 
+ * faction that returns value of x rise to the power of y
+ * @x: integer
+ * @y: integer
+ *
+ * return: power
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y != 0)
+	{
+		return (x* _pow_recursion(x, y - 1));
+	}
+	else 
+	{
+		return (1);
+	}
+}
