fe0cc4526591858c42249fa07eeb46bc0410506e
Maibarewa <najibusmanalasan@gmail.com>
2022-10-02 00:48:19 -0700

    std library

--- 0x09-static_libraries/_putchar.c
+++ 0x09-static_libraries/_putchar.c
@@ -1,1 +1,11 @@
+#include <unistd.h>
 
+/**
+ * _putchar - write the character c to the stdout
+ * @c: The chracter to print
+ * Return: on success 1.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
