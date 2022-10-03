931844e6647691767edc101cfdcc2239bcfa49e2
Maibarewa <najibusmanalasan@gmail.com>
2022-10-03 06:58:19 -0700

    Multiplication

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - The program multiplies numbers.
+ * followed by new line
+ *
+ * @argc: This is the argument count
+ * @argv: This is the argument vector
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int n; multiplication;
+
+	multiplication = 1;
+	if (argc < 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	for (n = 0; n < argc; n++)
+	{
+		multiplication = multiplication * atoi(argv[n]);
+	}
+	printf("%d\n", multiplication);
+	return (0);
+}
