3d92ce935974738a64a7b99c0f4c79ea6a13ace9
Maibarewa <najibusmanalasan@gmail.com>
2022-10-05 18:05:51 -0700

    alloc grid

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -31,7 +31,7 @@ int **alloc_grid(int width, int height)
 			p[i] = (int *) malloc(width * sizeof(int));
 			if (!p[i])
 			{
-				for (j = 0; j <= i j++)
+				for (j = 0; j <= i; j++)
 					free(p[i]);
 				free(p);
 				return (NULL);
