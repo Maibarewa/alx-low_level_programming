7030ba4ac758c91a965725ad767fd207de0e95dd
Maibarewa <najibusmanalasan@gmail.com>
2022-10-16 05:31:16 -0700

    task 0

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,3 @@
-#ifndef _FUNCTION_POINTERS_
-#define _FUNCTION_POINTERS_
-
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
@@ -10,6 +7,4 @@ int op_sub(int a, int b);
 int op_mul(int a, int b);
 int op_div(int a, int b);
 int op_mod(int a, int b);
-int (*get_op_func(char *s))(int, int);
-
-#endif /*_FUNCTION_*/
+int (*get_op_func(char *s);
