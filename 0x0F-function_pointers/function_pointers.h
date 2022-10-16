6e0a8f1ad6e39c184a0a969ec023a1d99dda8ad7
Maibarewa <najibusmanalasan@gmail.com>
2022-10-16 05:36:15 -0700

    task 0

--- 0x0F-function_pointers/function_pointers.h
+++
@@ -1,9 +0,0 @@
-int _putchar(char c);
-void print_name(char *name, void (*f)(char *));
-int int_index(int *array, int size, int (*cmp)(int));
-int op_add(int a, int b);
-int op_sub(int a, int b);
-int op_mul(int a, int b);
-int op_div(int a, int b);
-int op_mod(int a, int b);
-int (*get_op_func(char *s);
