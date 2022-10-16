47683a57fbcf1433df8b4a475b07a21fada3f34f
Maibarewa <najibusmanalasan@gmail.com>
2022-10-16 05:55:54 -0700

    task 2

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,4 +1,4 @@
-#include "function_poiters.h"
+#include "function_pointers.h"
 
 /**
  * array_iterator - executes a function given as a
@@ -13,6 +13,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 	unsigned int i;
 
 	if (array && action)
+
 		for (i = 0; i < size; i++)
 			action(array[i]);
 }
