9a43a00bf65a5c3a25064a328fcd3a3a78a1e861
Maibarewa <najibusmanalasan@gmail.com>
2022-10-27 09:48:02 -0700

    get bit

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -11,10 +11,10 @@ int get_bit(unsigned long int n, unsigned int index)
 	unsigned long int divisor, check;
 
 	if (index > (sizeof(unsigned long int) * 8 - 1))
-		return(-1);
+		return (-1);
 	divisor = 1 << index;
 	check = n & divisor;
 	if (check == divisor)
-		return(1);
-	return(0);
+		return (1);
+	return (0);
 }
