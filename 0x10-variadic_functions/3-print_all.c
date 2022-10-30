3d0b7ace882b337674bd176e2d30dd0f19998138
Maibarewa <najibusmanalasan@gmail.com>
2022-10-30 05:12:24 -0700

    print anything

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -20,11 +20,13 @@ void print_all(const char * const format, ...)
 	{
 		j = 0;
 		while (t_arg[j])
+		{
 			if (format[i] == t_arg[i] && c)
 			{
-				printf(",");
+				printf(", ");
 				break;
 			} j++;
+		}
 		switch (format[i])
 		{
 		case 'c':
@@ -32,6 +34,9 @@ void print_all(const char * const format, ...)
 			break;
 		case 'i':
 			printf("%d", va_arg(valist, int)), c = 1;
+			break;
+		case 'f':
+			printf("%f", va_arg(valist, double)), c = 1;
 			break;
 		case 's':
 			str = va_arg(valist, char *), c = 1;
